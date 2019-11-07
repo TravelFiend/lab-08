996c031caa50e4583b34f29fd7126d2ecc8d5391
Tali Avni <tali@aristo7.com>
2019-11-07 10:28:27 -0800

    api + starter server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-
 // Load Environment Variables from the .env file
-
+require('dotenv').config();
 
 // Application Dependencies
 const express = require('express');
-// (add cors, pg, and morgan...)
-
+const cors = require('cors');
+const pg = require('pg');
+const morgan = require('morgan');
 // Database Client
-// (create and connect using DATABASE_URL)
-
+const Client = pg.Client;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
 // Application Setup
 const app = express();
-// (add middleware utils: logging, cors, static files from public)
-// app.use(...)
+const PORT = process.send.PORT;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static('public'));
+
 
 
 // API Routes
