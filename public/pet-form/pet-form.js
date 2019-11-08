7b3def5e958261e36728a5fd2862cb9a609d4f56
TravelFiend <mikegrace274@gmail.com>
2019-11-08 00:05:55 -0800

    header done form started

--- public/pet-form/pet-form.js
+++ public/pet-form/pet-form.js
@@ -1,4 +1,4 @@
-import App from './YourResourceFormApp.js';
+import PetFormApp from './PetFormApp.js';
 
-const app = new App();
+const app = new PetFormApp();
 document.body.prepend(app.renderDOM());
