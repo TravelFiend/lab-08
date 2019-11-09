a51ecd59457445776f0dc96a2132f2cccb5c1d95
TravelFiend <mikegrace274@gmail.com>
2019-11-09 10:25:06 -0800

    pictures showing and basic styling done

--- data/pets.js
+++ data/pets.js
@@ -2,56 +2,56 @@ module.exports = [
     {
         name: 'Flying Fish',
         type: 'fish',
-        url: './assets/flying-fish.jpg',
+        url: '../assets/flying-fish.jpg',
         age: 1,
         flies: 'true'
     },
     {
         name: 'Chicken',
         type: 'bird',
-        url: './assets/chicken.jpg',
+        url: '../assets/chicken.jpg',
         age: 2,
         flies: 'false'
     },
     {
         name: 'Goldfish',
         type: 'fish',
-        url: './assets/goldfish.jpg',
+        url: '../assets/goldfish.jpg',
         age:  0,
         flies: 'false'
     },
     {
         name: 'Cockatoo',
         type: 'bird',
-        url: './assets/cockatoo.jpg',
+        url: '../assets/cockatoo.jpg',
         age: 1,
         flies: 'true'
     },
     {
         name: 'Tortoise',
         type: 'reptile',
-        url: './assets/tortoise.jpg',
+        url: '../assets/tortoise.jpg',
         age: 110,
         flies: 'false'
     },
     {
         name: 'Dry Frog',
         type: 'amphibian',
-        url: './assets/frog.jpg',
+        url: '../assets/frog.jpg',
         age: 5,
         flies: 'false'
     },
     {
         name: 'Snoopy',
         type: 'mammal',
-        url: './assets/dog.jpg',
+        url: '../assets/dog.jpg',
         age: 69,
         flies: 'true'
     },
     {
         name: 'Tiger',
         type: 'mammal',
-        url: './assets/tiger.jpg',
+        url: '../assets/tiger.jpg',
         age: 3,
         flies: 'false'
     }
